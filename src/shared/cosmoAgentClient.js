622f2233b194bdd5501375c43755848885b068f0
lmmcdev <subscription@clmmail.com>
2025-05-27 09:20:28 -0400

    Create cosmoAgentClient.js

---
+++ src/shared/cosmoAgentClient.js
@@ -0,0 +1,16 @@
+const { CosmosClient } = require("@azure/cosmos");
+
+const endpoint = process.env.COSMOS_ENDPOINT;
+const key = process.env.COSMOS_KEY;
+const databaseId = process.env.COSMOS_DATABASE_ID;
+const containerId = process.env.COSMOS_AGENT_CONTAINER_ID;
+
+const client = new CosmosClient({ endpoint, key });
+
+const getContainer = () => {
+  const database = client.database(databaseId);
+  return database.container(containerId);
+};
+
+module.exports = { getContainer };
+
