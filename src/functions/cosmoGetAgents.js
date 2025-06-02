ae4d17b10b88199be918d4950f89b5b4f5803c4d
lmmcdev <subscription@clmmail.com>
2025-06-02 10:34:48 -0400

    Update cosmoGetAgents.js

--- src/functions/cosmoGetAgents.js
+++ src/functions/cosmoGetAgents.js
@@ -1,4 +1,3 @@
-// src/functions/cosmoGet.js
 const { app } = require('@azure/functions');
 const { getAgentContainer } = require('../shared/cosmoAgentClient');
 const { success, error } = require('../shared/responseUtils');
@@ -8,14 +7,30 @@ app.http('cosmoGetAgents', {
   authLevel: 'anonymous',
   handler: async (req, context) => {
     try {
+      const assignedDepartment = req.query.get('assigned_department');
       const container = getAgentContainer();
 
-      const querySpec = {
-        query: `
-          SELECT c.id, c.agent_name,c.agent_email,c.agent_rol,c.agent_department,c.remote_agent,c.timestamp
-          FROM c
-        `
-      };
+      let querySpec;
+
+      if (assignedDepartment) {
+        querySpec = {
+          query: `
+            SELECT c.id, c.agent_name, c.agent_email, c.agent_rol,
+                   c.agent_department, c.remote_agent, c.timestamp
+            FROM c
+            WHERE c.agent_department = @dept
+          `,
+          parameters: [{ name: "@dept", value: assignedDepartment }]
+        };
+      } else {
+        querySpec = {
+          query: `
+            SELECT c.id, c.agent_name, c.agent_email, c.agent_rol,
+                   c.agent_department, c.remote_agent, c.timestamp
+            FROM c
+          `
+        };
+      }
 
       const { resources: items } = await container.items.query(querySpec).fetchAll();
       return success(items);
