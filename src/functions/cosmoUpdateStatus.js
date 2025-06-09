5e8e79619bbcf8daa679af09d40385961a44577b
lmmcdev <subscription@clmmail.com>
2025-06-09 16:13:07 -0400

    Update cosmoUpdateStatus.js

--- src/functions/cosmoUpdateStatus.js
+++ src/functions/cosmoUpdateStatus.js
@@ -3,6 +3,8 @@ const { getContainer } = require('../shared/cosmoClient');
 const { getAgentContainer } = require('../shared/cosmoAgentClient');
 const { success, badRequest, notFound, error } = require('../shared/responseUtils');
 
+const signalRUrl = process.env.SIGNAL_BROADCAST_URL2;
+
 app.http('cosmoUpdateStatus', {
   methods: ['PATCH'],
   authLevel: 'anonymous',
@@ -71,6 +73,43 @@ app.http('cosmoUpdateStatus', {
       });
 
       await item.patch(patchOps);
+
+      const { resource: updated } = await item.read();
+
+      const responseData = {
+        id: updated.id,
+        summary: updated.summary,
+        call_reason: updated.call_reason,
+        creation_date: updated.creation_date,
+        patient_name: updated.patient_name,
+        patient_dob: updated.patient_dob,
+        caller_name: updated.caller_name,
+        callback_number: updated.callback_number,
+        caller_id: updated.caller_id,
+        call_cost: updated.call_cost,
+        notes: updated.notes,
+        collaborators: updated.collaborators,
+        url_audio: updated.url_audio,
+        assigned_department: updated.assigned_department,
+        assigned_role: updated.assigned_role,
+        caller_type: updated.caller_type,
+        call_duration: updated.call_duration,
+        status: updated.status,
+        agent_assigned: updated.agent_assigned,
+        tiket_source: updated.tiket_source,
+        phone: updated.phone,
+        work_time: updated.work_time
+      };
+
+      try {
+        await fetch(signalRUrl, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(responseData)
+        });
+      } catch (e) {
+        context.log('⚠️ SignalR failed:', e.message);
+      }
 
       return success('Status updated successfully.', {
         applied_operations: patchOps.length
