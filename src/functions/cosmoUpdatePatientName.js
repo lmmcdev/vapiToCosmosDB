7046d9148bbc476a56e71b0bdb4629c453220e32
lmmcdev <subscription@clmmail.com>
2025-06-09 15:41:39 -0400

    Update cosmoUpdatePatientName.js

--- src/functions/cosmoUpdatePatientName.js
+++ src/functions/cosmoUpdatePatientName.js
@@ -113,14 +113,12 @@ app.http('cosmoUpdatePatientName', {
         work_time: updated.work_time
       };
 
-      console.log(signalRUrl)
       try {
         await fetch(signalRUrl, {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(responseData)
         });
-        console.log('sent it')
       } catch (e) {
         context.log('⚠️ SignalR failed:', e.message);
       }
