a6fd4bccb6bfc3ef87f410b03de7430634de2cb9
lmmcdev <subscription@clmmail.com>
2025-06-19 10:31:01 -0400

    Create cosmoSearchProviders.js

---
+++ src/functions/cosmoSearchProviders.js
@@ -0,0 +1,53 @@
+// src/functions/searchProviders.js
+const { app } = require('@azure/functions');
+const { success, error, badRequest } = require('../shared/responseUtils');
+
+const congnitiveURL = process.env.COGNITIVE_AI_URL;
+const cognitiveKEY = process.env.COGNITIVE_AI_API_KEY;
+
+
+app.http('searchProviders', {
+  methods: ['POST'],
+  authLevel: 'anonymous',
+  handler: async (request, context) => {
+    let body;
+    try {
+      body = await request.json();
+    } catch (err) {
+      return badRequest('Invalid JSON', err.message);
+    }
+
+    const requiredFields = ['query'];
+
+    const missingFields = requiredFields.filter(field => !body?.[field]);
+    
+
+    if (missingFields.length > 0) {
+      return badRequest(`Missing required fields: ${missingFields.join(', ')}`);
+    }
+
+    const query = body.query
+
+    //const searchEndpoint = 'https://cognitivesearchcservices.search.windows.net';
+    //const apiKey = '';
+    const indexName = 'providerscname-index';
+
+    try {
+      const response = await fetch(`${congnitiveURL}/indexes/${indexName}/docs?api-version=2023-10-01-Preview&search=${query}`,{
+          method: 'GET',
+          headers: { 'api-key': cognitiveKEY },
+        });
+        
+        if (!response.ok) {
+            throw new Error(`Search failed: ${response.status} ${response.statusText}`);
+        }
+
+        const data = await response.json();
+
+        return success('Search done', data, 201);
+    } catch (err) {
+      context.log(err.message);
+      return error('DB Insert error', 500, err.message);
+    }
+  }
+});
