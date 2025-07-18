2e45e5256901535fe7b65c309d7b830c867e9df4
lmmcdev <subscription@clmmail.com>
2025-07-18 11:32:49 -0400

    Update cosmoPatientsClient.js

--- src/shared/cosmoPatientsClient.js
+++ src/shared/cosmoPatientsClient.js
@@ -7,10 +7,28 @@ const containerId = "patients_id";
 
 const client = new CosmosClient({ endpoint, key });
 
-const getContainer = () => {
+const getPatientsContainer = () => {
   const database = client.database(databaseId);
   return database.container(containerId);
 };
 
 module.exports = { getPatientsContainer };
 
+
+
+//LOCALHOST
+/*const { CosmosClient } = require("@azure/cosmos");
+
+const endpoint = "https://lmmccosmos02.documents.azure.com:443/";
+const key = ""; // la clave primaria
+
+const client = new CosmosClient({
+  endpoint,
+  key
+});
+
+const getPatientsContainer = () => {
+  const db = client.database("IAData");
+  return db.container("patients_id");
+};
+module.exports = { getPatientsContainer };*/
