d9548f2644fea2bf0fb4357a8ee0b332f2ff6022
lmmcdev <suscription@clmmail.com>
2025-08-07 12:52:58 -0400

    Update cosmoGetQuality.js

--- src/functions/cosmoGetQuality.js
+++ src/functions/cosmoGetQuality.js
@@ -1,5 +1,5 @@
 const { app } = require('@azure/functions');
-const { getContainer } = require('../shared/cosmoClient');
+const { getQAContainer } = require('../shared/cosmoQAClient'); // contenedor QC
 const { getAgentContainer } = require('../shared/cosmoAgentClient');
 const { success, badRequest, error } = require('../shared/responseUtils');
 
@@ -12,7 +12,7 @@ app.http('cosmoGetQuality', {
       if (!agentEmail) return badRequest("Missing 'agent_assigned' in query.");
 
       const agentContainer = getAgentContainer();
-      const ticketContainer = getContainer();
+      const ticketContainer = getQAContainer();
 
       // ✅ Buscar agente
       const { resources: agentResult } = await agentContainer.items
@@ -28,42 +28,14 @@ app.http('cosmoGetQuality', {
 
       let query, parameters;
 
-      if (agent_rol === "Quality") {
+      if (agent_rol !== "Quality") return
+
+     
         query = `
-          SELECT c.id, c.summary, c.call_reason, c.creation_date, c.patient_name,
-                 c.patient_dob, c.caller_name, c.callback_number, c.caller_id,
-                 c.call_cost, c.notes, c.collaborators, c.url_audio, c.assigned_department,
-                 c.assigned_role, c.caller_type, c.call_duration, c.status, c.agent_assigned,
-                 c.tiket_source, c.phone, c.work_time, c.aiClassification, c.createdAt,
-                 c.patient_id, c.linked_patient_snapshot
+          SELECT *
           FROM c
-          WHERE c.assigned_department = @department
-            AND LOWER(c.status) != "done"
         `;
-        parameters = [
-          { name: "@department", value: agent_department }
-        ];
-      } else {
-        query = `
-          SELECT c.id, c.summary, c.call_reason, c.creation_date, c.patient_name,
-                 c.patient_dob, c.caller_name, c.callback_number, c.caller_id,
-                 c.call_cost, c.notes, c.collaborators, c.url_audio, c.assigned_department,
-                 c.assigned_role, c.caller_type, c.call_duration, c.status, c.agent_assigned,
-                 c.tiket_source, c.phone, c.work_time, c.aiClassification, c.createdAt,
-                 c.patient_id, c.linked_patient_snapshot
-          FROM c
-          WHERE (
-                  (c.agent_assigned = @agentEmail OR c.agent_extension = @agent_extension)
-                  OR (c.agent_assigned = "" AND c.assigned_department = @department)
-                )
-            AND LOWER(c.status) != "done"
-        `;
-        parameters = [
-          { name: "@agentEmail", value: agentEmail },
-          { name: "@department", value: agent_department },
-          { name: "@agent_extension", value: agent_extension }
-        ];
-      }
+      
 
       const { resources: tickets } = await ticketContainer.items
         .query({ query, parameters })
