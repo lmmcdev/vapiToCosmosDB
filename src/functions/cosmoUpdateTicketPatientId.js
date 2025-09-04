a42daf8d925aa9a10f7f2fc28558b2aba4267814
lmmcdev <suscription@clmmail.com>
2025-09-04 11:38:11 -0400

    Update cosmoUpdateTicketPatientId.js

--- src/functions/cosmoUpdateTicketPatientId.js
+++ src/functions/cosmoUpdateTicketPatientId.js
@@ -69,6 +69,7 @@ app.http('updateTicketsByPhone', {
               DOB: patient.DOB || '',
               Address: patient.Address || '',
               Location: patient.Location_Name || '',
+              Attending_Physician: patient.Attending_Physician || '',
             };
           }
         } catch (err) {
